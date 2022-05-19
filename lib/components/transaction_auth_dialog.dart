136ef0175a650005b7b1e8eee5468833e5182967
lzc313 <leonardo.zc@live.com>
2022-05-19 17:44:05 -0300

    Projeto inicial

---
+++ lib/components/transaction_auth_dialog.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TransactionAuthDialog extends StatelessWidget {
+  const TransactionAuthDialog({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
